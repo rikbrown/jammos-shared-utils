e29fd42035b6dedb73d88d2f8f9560d7ebd7de54
Rik Brown <rik@rik.email>
2017-11-02 22:17:48 -0700

    Remove DataInputOutput and replace with ByteBuf support

--- src/main/kotlin/net/jammos/utils/extensions/ByteBufExtensions.kt
+++ src/main/kotlin/net/jammos/utils/extensions/ByteBufExtensions.kt
@@ -1,10 +1,17 @@
 package net.jammos.utils.extensions
 
+import com.google.common.net.InetAddresses
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.ByteBufInputStream
 import io.netty.buffer.ByteBufOutputStream
+import net.jammos.utils.types.BigUnsignedInteger
+import net.jammos.utils.types.WriteableByte
 import java.io.DataInput
 import java.io.DataOutput
+import java.net.InetAddress
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 
 fun ByteBuf.asDataOutput(): DataOutput {
     return ByteBufOutputStream(this)
@@ -12,4 +19,51 @@ fun ByteBuf.asDataOutput(): DataOutput {
 
 fun ByteBuf.asDataInput(): DataInput {
     return ByteBufInputStream(this)
+}
+
+fun ByteBuf.readCharsUntil(until: Byte): String {
+    val sb = StringBuilder()
+    while (true) {
+        val byte = readByte()
+        if (byte == until) {
+            break
+        }
+        sb.append(byte.toChar())
+    }
+    return sb.toString()
+    //return if (reverse) sb.toString().reversed() else sb.toString()
+}
+
+fun ByteBuf.readCharSequence(cnt: Int) = readCharSequence(cnt, Charsets.US_ASCII)
+
+fun ByteBuf.readByteArray(count: Int): ByteArray {
+    val bytes = ByteArray(count)
+    readBytes(bytes)
+    return bytes
+}
+
+fun ByteBuf.readIpAddress(): InetAddress {
+    /** [InetAddresses.fromInteger] because [InetAddresses.fromLittleEndianByteArray] forces little endian].
+     * and we use this because [InetAddress] can potentially make network calls on construction...
+     */
+    return InetAddresses.fromInteger(readInt())
+}
+
+fun ByteBuf.readBigUnsigned(size: Int): BigUnsignedInteger {
+    val bytes = readByteArray(size)
+    return BigUnsignedInteger(bytes)
+}
+
+fun ByteBuf.writeByte(writeableByte: WriteableByte) = writeByte(writeableByte.value)
+
+fun ByteBuf.writeByte(short: Short) = writeByte(short.toInt()) // FIXME: write better
+
+fun ByteBuf.writeCharSequenceTerminated(string: String, terminator: Int = 0) {
+    writeBytes(string.toByteArray(Charsets.US_ASCII))
+    writeByte(terminator)
+}
+
+// TODO: remove when Netty adds this
+fun ByteBuf.writeFloatLE(value: Float) {
+    writeBytes(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putFloat(value).array())
 }
---
+++ src/main/kotlin/net/jammos/utils/types/Enums.kt
@@ -0,0 +1,12 @@
+package net.jammos.utils.types
+
+abstract class ReversibleEnum<out T: Any, in V>(values: Array<T>, reverser: (T) -> V) {
+    private val byValue = values.associateBy { reverser(it) }
+
+    fun ofValue(value: V) = ofValueOrNull(value) ?: throw IllegalArgumentException("Unknown value: " + value)
+    fun ofValueOrNull(value: V) = byValue[value]
+}
+
+abstract class ReversibleByte<out T: WriteableByte>(values: Array<T>): ReversibleEnum<T, Int>(values, WriteableByte::value) {
+    fun ofValue(value: Short) = ofValue(value.toInt())
+}
